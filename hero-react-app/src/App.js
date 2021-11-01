170e34cb4f87c9d731348215fc001e02d5d1e8e2
MinhDuc7749 <55194555+MinhDuc7749@users.noreply.github.com>
2021-11-01 23:20:29 +0700

    Tour hero

---
+++ hero-react-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import HerosPage from './components/HerosPage';
+
+function App() {
+  return (
+    <div className="App">
+      <HerosPage/>
+    </div>
+  );
+}
+
+export default App;
