fcb8fbfce0211da09988fa7c5c4470c831fb6e8d
MinhDuc7749 <55194555+MinhDuc7749@users.noreply.github.com>
2021-11-01 23:42:47 +0700

    Update HerosPage.js

--- hero-react-app/src/components/HerosPage.js
+++ hero-react-app/src/components/HerosPage.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+
 import React from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { CancelToken } from 'axios';
 import './HerosPage.css';
